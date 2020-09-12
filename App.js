6203a3f09ca253f90170fc2465ede4332a68c269
Michelson <nsmichelson@gmail.com>
2020-09-12 14:33:59 -0700

    added redux and middleware packages in the app file

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { setInitialData } from './utils/helper.js'
 import DeckDashboard from './components/DeckDashboard.js'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import reducer from './reducers/'
+import { createStore, applyMiddleware } from 'redux'
 
 export default function App() {
   return (
