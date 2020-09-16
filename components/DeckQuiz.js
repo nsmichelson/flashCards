c1bce19f85e48167c6bd9caf7e160c22522903fb
Michelson <nsmichelson@gmail.com>
2020-09-16 12:17:22 -0700

    making good progress on flash cards

--- components/DeckQuiz.js
+++ components/DeckQuiz.js
@@ -2,12 +2,47 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
-const DeckQuiz = (props) => {
-  return (
-    <View>
-      <Text style={{fontSize:30, color:'white'}}>Boom</Text>
-    </View>
-  )
-}
+class DeckQuiz extends React.Component {
+  state={
+    currentQuestion:0,
+    numCorrect:0,
+    showAnswer:false
+  }
+  render(){
+    console.log("props props props props", this.props)
+    const { deck } = this.props.route.params
+    const questionKeys = Object.keys(deck.questions)
 
-export default DeckQuiz
+    let { showAnswer, currentQuestion, numCorrect } = this.state
+    return (
+      <View>
+        {showAnswer===true ?
+          <Text style={{fontSize:30, color:'white'}}>{deck.questions[currentQuestion].answer}</Text>
+        :
+          <Text style={{fontSize:30, color:'white'}}>{deck.questions[currentQuestion].question}</Text>
+        }
+        <Button
+          onPress={this.onPressFlip}
+          title={showAnswer ? "Flip to Question" : "Flip to Answer"}
+          color="orange"
+          />
+        <View style={{flexDirection:'row'}}>
+          <Button
+            onPress={this.onPressCorrect}
+            title="Mark Correct"
+            color="green"
+            />
+          <Button
+            onPress={this.onPressCorrect}
+            title="Mark Incorrect"
+            color="red"
+            />
+        </View>
+      </View>
+      )
+    }
+  }
+
+
+
+export default connect()(DeckQuiz)
