8057a5816c66eac5908c03bdaa168751b14e55a7
Michelson <nsmichelson@gmail.com>
2020-09-16 21:01:40 -0700

    finishing up add deck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet,Button, TextInput} from 'react-native'
 
 export default class AddDeck extends React.Component {
-  render(){
-  return(
-    <View></View>
-    )
+  state={
+    text:''
   }
-}
+  handleChangeText= ({text}) =>{
+    this.setState(()=>{
+      return({
+        text:text
+      })
+    })
+    }
+  render(){
+    return(
+      <View>
+        <View style={{fontSize:30,color:'white'}}>
+            <TextInput
+              style={{fontSize:30,color:'blue',backgroundColor:'white'}}
+              value={this.state.text}
+              onChangeText={this.handleChangeText}
+            />
+          </View>
+
+      </View>
+      )
+    }
+  }
